d41024a430b181bb80bc7ba7385ebfb2717c1b46
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2026-06-10 02:55:34 -0400

    added the isvisible and ragestered at field in the model

--- src/main/java/com/pluralsight/demo/internship/model/Candidate.java
+++ src/main/java/com/pluralsight/demo/internship/model/Candidate.java
@@ -2,6 +2,8 @@ package com.pluralsight.demo.internship.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "candidates")
 public class Candidate {
@@ -15,7 +17,8 @@ public class Candidate {
     private String email;
     
     private String fieldOfStudy;
-
+    private LocalDateTime registeredAt;
+    private boolean visible;
     // Constructors
     public Candidate() {
     }
@@ -58,4 +61,16 @@ public class Candidate {
     public void setFieldOfStudy(String fieldOfStudy) {
         this.fieldOfStudy = fieldOfStudy;
     }
-}
+    public LocalDateTime getRegisteredAt() {
+        return registeredAt;
+    }
+    public void setRegisteredAt(LocalDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+    public boolean isVisible() {
+        return visible;
+    }
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+    }
--- src/main/java/com/pluralsight/demo/internship/service/CandidateService.java
+++ src/main/java/com/pluralsight/demo/internship/service/CandidateService.java
@@ -2,8 +2,10 @@ package com.pluralsight.demo.internship.service;
 
 import com.pluralsight.demo.internship.model.Candidate;
 import com.pluralsight.demo.internship.repository.CandidateRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +13,8 @@ import java.util.stream.Collectors;
 public class CandidateService {
 
     private final CandidateRepository candidateRepository;
-
+    @Value("${candidates.visible-by-default}")
+    private boolean visibleByDefault;
     public CandidateService(CandidateRepository candidateRepository) {
         this.candidateRepository = candidateRepository;
     }
@@ -38,6 +41,8 @@ public class CandidateService {
     }
 
     public Candidate createCandidate(Candidate candidate) {
+        candidate.setRegisteredAt(LocalDateTime.now());
+        candidate.setVisible(visibleByDefault);
         return candidateRepository.save(candidate);
     }
 
